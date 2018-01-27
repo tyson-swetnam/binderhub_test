f7e96bd38786449b9a1cee46844e6cc7fe36f05f
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2018-01-26 17:22:17 -0700

    Update ubuntu16_build.sh

--- ubuntu16_build.sh
+++ ubuntu16_build.sh
@@ -66,3 +66,33 @@ sudo helm install jupyterhub/binderhub --version=v0.9.0dev --name=binder --names
 # To add a node
 # sudo kubeadm join --token TOKEN MASTER_IP:6443
 
+# Install Minikube with KVM2 driver
+# https://github.com/kubernetes/minikube/blob/master/docs/drivers.md#kvm-driver
+# Install libvirt and qemu-kvm on your system, e.g.
+# Debian/Ubuntu (for Debian Stretch libvirt-bin it's been replaced with libvirt-clients and libvirt-daemon-system)
+# sudo apt install -y libvirt-bin qemu-kvm
+# sudo usermod -a -G libvirtd $(whoami)
+# newgrp libvirtd
+# curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 && chmod +x docker-machine-driver-kvm2 && sudo mv docker-machine-driver-kvm2 /usr/bin/
+
+# curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+
+# export MINIKUBE_WANTUPDATENOTIFICATION=false
+# export MINIKUBE_WANTREPORTERRORPROMPT=false
+# export MINIKUBE_HOME=$HOME
+# export CHANGE_MINIKUBE_NONE_USER=true
+# mkdir $HOME/.kube || true
+# touch $HOME/.kube/config
+
+# export KUBECONFIG=$HOME/.kube/config
+# sudo -E ./minikube start --vm-driver=kvm2
+
+# this for loop waits until kubectl can access the api server that Minikube has created
+# for i in {1..150}; do # timeout for 5 minutes
+#   ./kubectl get po &> /dev/null
+#   if [ $? -ne 1 ]; then
+#       break
+#  fi
+#  sleep 2
+# done
+# kubectl commands are now able to interact with Minikube cluster
